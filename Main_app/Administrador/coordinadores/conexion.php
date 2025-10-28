9b1f07cf592972f9c561276503d78963449ed1f9
Carlos270512 <carlosgarces270512@gmail.com>
2025-10-28 13:12:31 -0500

    primer commit

--- Main_app/Administrador/coordinadores/conexion.php
+++ Main_app/Administrador/coordinadores/conexion.php
@@ -4,14 +4,10 @@ $dbname = 'desaroll_appautoevaluacionbd';
 $user = 'desaroll_appUsuario';
 $password = '=v=J7WL@V8zT';
 
-// Crear la conexi¨®n
-$mysqli = new mysqli($host, $user, $password, $dbname);
-
-// Verificar si hay error de conexi¨®n
-if ($mysqli->connect_error) {
-    die('Error: No se pudo conectar a la base de datos. ' . $mysqli->connect_error);
+try {
+    $con = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Error: No se pudo conectar a la base de datos. ' . $e->getMessage());
 }
-
-// Establecer el conjunto de caracteres
-$mysqli->set_charset("utf8mb4");
 ?>
